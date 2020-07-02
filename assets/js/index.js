7cafb7fb0e6624fe2a10030de97e944ca9905c73
Raghuram <1993raghuram@gmail.com>
2020-07-02 11:47:15 +0530

    Add responsiveness

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,7 +24,7 @@ class BhagvadGita{
                     <div class="dropdown-verse-root"></div>
                     <div class="form-group">
                         <label for="verse-input">Verse Number</label>
-                        <input class="verse-number" type="number" placeholder="Verse no. ex: 24">
+                        <input disabled class="verse-number" type="number" placeholder="Verse no. ex: 24">
                     </div>
                     <button  class="btn btn-submit">Submit</button>
                 </form>
@@ -40,9 +40,7 @@ class BhagvadGita{
     }
 
     handlers(){
-        // setInterval(async () => {
-        //     this.authKey = await this.getAuthToken()
-        // },300000)
+        
 
         this.verseForm_DOM = document.querySelector(".verse-form");
         this.chapterForm_DOM = document.querySelector(".chapter-form");
@@ -57,10 +55,11 @@ class BhagvadGita{
             this.handleClickOnChapterSubmitBtn(e)
         })
 
+        document.body.addEventListener("click",async (e) => {
+            await this.handleClickOnSelectingChapterInVerseDropdown(e)
+        })
+
        window.addEventListener("load",(e) => {
-
-            // this.authKey = localStorage.getItem("authKey") || 
-
             // By default  fetch verse number for selected chapter;
             const selectedChapter = this.verseForm_DOM.querySelector(".selected").dataset.value;
             this.getValidVerseNumbers(selectedChapter);
@@ -68,7 +67,18 @@ class BhagvadGita{
 
     }
 
+    async handleClickOnSelectingChapterInVerseDropdown(e){
+        const element = e.target.closest(".verse-form .custom-option");
+        if(!element) return;
+        const selectedChapter = element.dataset.value;
+        const verseInput_DOM = document.querySelector(".verse-number");
+        verseInput_DOM.setAttribute("disabled",true);
+        await this.getValidVerseNumbers(selectedChapter);
+        verseInput_DOM.removeAttribute("disabled")
+    }
+
     async getValidVerseNumbers(selectedChapter){
+        
         let tokenPromise = await this.getAuthToken();
         tokenPromise = await tokenPromise.json();
         let token = tokenPromise.access_token;
@@ -77,14 +87,65 @@ class BhagvadGita{
         const chapterVerseNumber = chapter.verses_count;
 
         const verseInput_DOM = document.querySelector(".verse-number");
-        // verseInput_DOM.setAttribute("placeholder",)
-
+        verseInput_DOM.setAttribute("placeholder",`Enter between 1 - ${chapterVerseNumber}`);
+        verseInput_DOM.removeAttribute("disabled");
+        localStorage.setItem("verseNo",chapterVerseNumber);
+        localStorage.setItem("authKey", token);
 
     }
 
+    clearVerseForm(){
+        const verseInput_DOM = document.querySelector(".verse-number");
+        verseInput_DOM.value = "";
+        verseInput_DOM.setAttribute("placeholder",`Enter between 1 - ${localStorage.getItem("verseNo")}`);
+    }
+
     async handleClickOnVerseSubmitBtn(e){
+     
+      this.moveLayoutToLeftSide();
+      this.contentContainer_DOM.innerHTML = `
+            <img class="loading" src="./assets/media/spinner.gif" alt="loader">
+      `;
+      const chapterNo = document.querySelector(".verse-form .selected").dataset.value;
+      const verseNo = document.querySelector(".verse-number").value;
+
+      let tokenPromise = await this.getAuthToken();;
+      tokenPromise = await tokenPromise.json();
+      let token = tokenPromise.access_token;
+
+      if(+verseNo < 1 || +verseNo > +localStorage.getItem("verseNo") ||  verseNo.includes(".")) {
+          this.contentContainer_DOM.innerHTML = `
+            <div>
+                Invalid Verse Number
+            </div>
+          `
+          return;
+      }
+
+
+      document.querySelector(".loading").style.display = "block";
+      let getVerse = await fetch(`https://bhagavadgita.io/api/v1/chapters/${chapterNo}/verses/${verseNo}?access_token=${token}`);
+      document.querySelector(".loading").style.display = "none";
+      getVerse = await getVerse.json();
+      this.clearVerseForm()
+
+      if(getVerse.chapter_number){
+            this.contentContainer_DOM.innerHTML = `
+            <h4 class="verse-title">Chapter : ${getVerse.chapter_number} - Verse: ${getVerse.verse_number}</h4>
+            <h2 class="verse">${getVerse.text}<h2>
+            <h4 class="verse-transliteration">${getVerse.transliteration}</h4>   
+            <h3 class="verse-meaning">${getVerse.meaning}</h3>
+            <p class="verse-word-meaning">
+            <strong>Word Meanings </strong>: ${getVerse.word_meanings}
+            </p>
+        `
+      } else {
+          this.contentContainer_DOM.innerHTML =    `
+            Chapter : ${chapterNo} - Verse : ${verseNo} not found
+          `
+      }
+     
       
-        // const response = await fetch(url)
     }
 
     async getAuthToken(){
@@ -104,7 +165,8 @@ class BhagvadGita{
 
     moveLayoutToLeftSide(){
         //moves layout to leftside and chapter-content to right-side.
-        document.querySelector(".modal").style.transform = "translateX(-30%)";
+        // document.querySelector(".modal").style.transform = "translateX(-30%)";
+        document.querySelector(".modal").classList.add("modal-move-left");
         document.querySelector(".chapter-content").classList.add("chapter-content-display");
     }
 
@@ -114,14 +176,14 @@ class BhagvadGita{
         const token = tokenPromise.access_token;
 
         const selectedChapter = e.target.querySelector(".chapter-form .selected").dataset.value;
+        this.contentContainer_DOM.innerHTML = `
+            <img class="loading" src="./assets/media/spinner.gif" alt="loader">
+        `;
 
         document.querySelector(".loading").style.display = "block";
         const fetchChapterDetails = await fetch(`https://bhagavadgita.io/api/v1/chapters/${selectedChapter}?access_token=${token}`).then(data => data.json());
         document.querySelector(".loading").style.display = "none";
 
-        this.contentContainer_DOM.innerHTML = `
-            <img class="loading" src="./assets/media/spinner.gif" alt="loader">
-        `;
         let chapter = fetchChapterDetails;
         this.contentContainer_DOM.innerHTML += `
                 <h2 class="chapter-title">${chapter.chapter_number} - ${chapter.name}
